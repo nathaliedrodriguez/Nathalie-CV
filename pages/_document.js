24d4dc40f0a1060f8aec7acf67601d76eab48f7b
Nathalie D. Rodriguez <116244727+nathaliedrodriguez@users.noreply.github.com>
2025-05-20 15:20:22 -0300

    Add Google Analytics via _document.js

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-YLPPP5YFRY`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-XXXXXXXXXX');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
