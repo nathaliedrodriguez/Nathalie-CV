a0b1370a1e875f5eb920a5b54b4e21d6afb8002e
Nathalie D. Rodriguez <116244727+nathaliedrodriguez@users.noreply.github.com>
2025-05-22 22:03:55 -0300

    Add Script package to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { El_Messiri, Epilogue } from "next/font/google"
+import Script from "next/script";
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import ScrollUp from "@/components/scroll-up"
