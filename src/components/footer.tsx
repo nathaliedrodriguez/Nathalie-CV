62a23a0893e09c5be121447031c5ada36b47983f
Fertmeneses_HPvictus <fertmeneses@gmail.com>
2025-06-11 21:42:23 -0300

    Try fixing the footer so it keeps the same width as the header

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function footer() {
   return (
-    <footer className="bg-[#f2f8fb] py-8 px-4 md:px-16 mt-8 -mx-3 md:-mx-8 rounded-t-[50px]">
+    // <footer className="bg-[#f2f8fb] py-8 px-4 md:px-16 mt-8 -mx-3 md:-mx-8 rounded-t-[50px]">
+    <footer className="bg-[#f2f8fb] py-8 px-4 md:px-16 mt-8 rounded-t-[50px]">
       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between pb-16">
         <div className="flex flex-col items-center gap-4 mb-4 md:mb-0">
           <img src="/HomePage/icons/star.png" className="w-24 h-24" alt="StarIcon" />
